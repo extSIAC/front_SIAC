109418414b955b97cc0a05546be5f6dd1b5006d6
extSIAC <110553034+extSIAC@users.noreply.github.com>
2023-08-16 23:02:05 -0600

    Delete grafico directory

--- grafico/controlador_grafico_actividades_culturales.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'modelo_grafico.php';
-    $MU = new Modelo_Grafico();
-    
-
-    $consulta = $MU->TraerDatosGraficoActividadesCulturales();
-    echo json_encode ($consulta);
-
--- grafico/controlador_grafico_actividades_deportivas.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'modelo_grafico.php';
-    $MU = new Modelo_Grafico();
-    
-
-    $consulta = $MU->TraerDatosGraficoActividadesDeportivas();
-    echo json_encode ($consulta);
-
